0f161f9a2ce5c0fa2259975dc19cd0d35ca9bb17
Myth80 <61623726+Myth80@users.noreply.github.com>
2026-01-23 15:16:20 +0530

    gemini services

--- backend/src/services/geminiService.js
+++ backend/src/services/geminiService.js
@@ -21,7 +21,7 @@ export const embedText = async (text) => {
    ========================= */
 export const askGemini = async (prompt) => {
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-flash-latest",
   });
 
   const result = await model.generateContent(prompt);
